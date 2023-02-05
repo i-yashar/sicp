8c570fc7c5a60085d650bdc64655a45c48482048
Ibrahim Yashar <ibo7777@abv.bg>
2023-02-05 13:27:28 +0200

    1.3

--- 1.3.scm
+++ 1.3.scm
@@ -2,3 +2,17 @@
   (cond ((and (> a c) (> b c)) (+ (* a a) (* b b)))
         ((and (> b a) (> c a)) (+ (* b b) (* c c )))
         ((and (> a b) (> c b)) (+ (* a a) (* c c )))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (sum-of-squares a b)
+  (+ (* a a)
+     (* b b)))
+
+(define (first-two-larger a b c)
+  (and (> a c) (> b c)))
+
+(define (f a b c)
+  (cond ((first-two-larger a b c) (sum-of-squares a b))
+        ((first-two-larger b c a) (sum-of-squares b c))
+        (else (sum-of-squares a c))))
